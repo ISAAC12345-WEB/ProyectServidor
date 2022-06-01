a6e667c720dd588140d6dec2afc178009a09fcba
ISAAC12345-WEB <89948658+ISAAC12345-WEB@users.noreply.github.com>
2022-06-01 22:31:09 +0200

    Mantenimiento Servidor

---
+++ src/main/java/com/empresa/servidor/controller/ClienteController.java
@@ -0,0 +1,45 @@
+package com.empresa.servidor.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.servidor.entity.Cliente;
+import com.empresa.servidor.service.ClienteService;
+
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping("/lista")
+	public List<Cliente> listar(){
+		return service.listAll();
+	}
+	
+	@PostMapping("/registra")
+	public void registra(@RequestBody Cliente cliente) {
+		service.save(cliente);
+	}
+	
+	@PutMapping("/actualiza")
+	public void actualiza(@RequestBody Cliente cliente) {
+		service.update(cliente);
+	}
+	
+	@DeleteMapping("/eliminia/{id}")
+	public void eliminia(@PathVariable("id") Integer id) {
+		service.delete(id);
+	}
+}
---
+++ src/main/java/com/empresa/servidor/entity/Cliente.java
@@ -0,0 +1,72 @@
+package com.empresa.servidor.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idcliente")
+	private int id;
+	private String dni;
+	private String nombres;
+	private String direccion;
+	private String estado;
+	
+	public Cliente() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", dni=" + dni + ", nombres=" + nombres + ", direccion=" + direccion + ", estado="
+				+ estado + "]";
+	}
+ 
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+}
